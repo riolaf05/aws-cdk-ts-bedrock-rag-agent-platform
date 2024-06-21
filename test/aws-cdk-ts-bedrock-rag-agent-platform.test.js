026a698007dcce21dd40f42a5ca62eb4bb6701ef
Rosario Laface <lafacerosario@gmail.com>
2024-06-21 13:32:16 +0200

    Initial commit

---
+++ test/aws-cdk-ts-bedrock-rag-agent-platform.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const AwsCdkTsBedrockRagAgentPlatform = require('../lib/aws-cdk-ts-bedrock-rag-agent-platform-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-ts-bedrock-rag-agent-platform-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new AwsCdkTsBedrockRagAgentPlatform.AwsCdkTsBedrockRagAgentPlatformStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
